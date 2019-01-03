9f0fe40629cf3c12485968d421df0cf00223d95c
hertogp <git.hertogp@gmail.com>
2019-01-03 13:28:12 +0100

    repoExists now checks in git file-tree if repo is being tracked already

--- setup.sh
+++ setup.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-# Setup 1 or more lib/<framework>
-# - adds external git repo's as submodules
-# - or updates them
+# See https://git-scm.com/book/en/v2/Git-Tools-Submodules
+
+# Setup/update 1 or more lib/<framework>
+
+# howto add another repo as submodule:
+#
+# 1- add REPONAME="url" to '- git repo urls' below
+# 2- add reponame to $REPOLIB list below
+
+# howto re-add a submodule after deleting it, using setup.sh
+#
+# 1- ./setup.sh <framework>
+#
+# - repoExists checks to see if we're tracking <framework> as a submodule
+#   and if so, will checkout the previously committed version of the repo.
+
 
 # globs
-# - git repos
-REVEAL="https://github.com/hakimel/reveal.js.git"
-IMPRESS="https://github.com/impress/impress.js.git"  # includes submodules
-INSPIRE="https://github.com/LeaVerou/inspire.js.git"
+REVEAL="https://github.com/hakimel/reveal.js.git"     # repo url
+IMPRESS="https://github.com/impress/impress.js.git"   # repo url
+INSPIRE="https://github.com/LeaVerou/inspire.js.git"  # repo url
+REPOLIB="reveal impress inspire"                      # supported frameworks
+REPODIR=lib                                           # where the frameworks go
 
-# - local dirs used
-REPOLIB="reveal impress inspire"      # supported frameworks
-REPODIR=lib
-
-# HELPERS
+# helpers
 usage() {
     echo "Usage:"
     echo
@@ -27,63 +37,49 @@ usage() {
 }
 
 repoUrl() {
-    # return URL for repo
-
-    case $1 in
-        reveal) echo $REVEAL;;
-        impress) echo $IMPRESS;;
-        inspire) echo $INSPIRE;;
+    # return URL for repo ($1)
+    case ${1} in
+        reveal) echo ${REVEAL};;
+        impress) echo ${IMPRESS};;
+        inspire) echo ${INSPIRE};;
         *) echo "";;
     esac
 }
 
 repoExists() {
-    # True REPO ($1) exists, False otherwise
-
-    if [ -d "${REPODIR}/${1}" ]; then
+    # True if REPO is presently being tracked, False otherwise
+    if git ls-files --error-unmatch ${REPODIR}/${1} 2>/dev/null 1>&2
+    then
         return 0;
     else
         return 1
     fi
 }
 
-repoOpts() {
-    # Return options for git submodule command
-
-    case $1 in
-        impress) echo "--recursive";;
-        *) echo "";;
-    esac
-}
-
 repoAdd() {
-    # add public repo as submodule to this repo
-
-    REPO=$1;
-    OPTS=$(repoOpts $REPO)
-    URL=$(repoUrl $1)
-    CMD1="git submodule add $URL $REPODIR/$REPO"
-    CMD2="git submodule update --init --recursive $REPODIR/$REPO"
+    # add a new public repo as submodule to this repo
+    REPO=${1};
+    URL=$(repoUrl ${REPO})
+    CMD1="git submodule add ${URL} ${REPODIR}/${REPO}"
     echo "repoAdd:"
-    echo "- $CMD1"
+    echo "> $CMD1"
     $CMD1
-
+    # if repo's .gitmodules is non-zero: add its sub-submodules as well
     if [ -s "$REPODIR/$REPO/.gitmodules" ]
     then
-        echo "- $CMD2"
+        CMD2="git submodule update --init --recursive ${REPODIR}/${REPO}"
+        echo "> $CMD2"
         $CMD2
     fi
 }
 
 repoUpdate() {
-    # Update rather than clone a repo
-    # $1 = framework name from $LIBS
-
+    # Update an existing submodule (repo)
     REPO=$1
-    OPTS=$(repoOpts $REPO)
-    CMD="git submodule update --remote --recursive ${REPODIR}/$REPO"
-
-    echo "repoUpdate: $CMD"
+    OPTS=$(repoOpts ${REPO})
+    CMD="git submodule update --remote --recursive ${REPODIR}/${REPO}"
+    echo "repoUpdate:"
+    echo "> $CMD"
     $CMD
 }
 
@@ -98,7 +94,7 @@ else
     for REPO in $@; do
         if [ -z "$(repoUrl $REPO)" ]
         then
-            echo "rejecting $REPO: -> must be 1 of [${REPOLIB}]"
+            echo "rejecting $REPO: -> not 1 of ${REPOLIB}"
         else
             echo "accepting $REPO"
             REPOS="${REPOS} $REPO"
@@ -118,4 +114,5 @@ for REPO in ${REPOS}; do
 
 done
 
+echo "Done."
 exit 0
