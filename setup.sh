811d6807ff04685f3e2ee307a3e9b6bf19efebe2
hertogp <git.hertogp@gmail.com>
2019-01-03 10:00:27 +0100

    setup testing

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # Setup 1 or more lib/<framework>
+# - adds external git repo's as submodules
+# - or updates them
 
 # globs
+# - git repos
+REVEAL="https://github.com/hakimel/reveal.js.git"
+IMPRESS="https://github.com/impress/impress.js.git"  # includes submodules
+INSPIRE="https://github.com/LeaVerou/inspire.js.git"
+
+# - local dirs used
 LIBS="reveal impress inspire"      # supported frameworks
+LIBS="reveal" # tmp for testing setup.sh (later we'll test with impress)
 LIBDIR=lib
 
 # HELPERS
@@ -18,41 +27,46 @@ usage() {
     echo "  - inspire"
 }
 
-gitclone() {
-
-    LIB=$1;
-    OPT=${2:-"opt"};
-    URL=${3:-"url"};
-
-    echo "git clone ${OPT} ${URL} ${LIBDIR}/${LIB}"
+repo() {
+    # return URL for repo
 }
 
-gitexists() {
-    # check if $1 refers to a lib/<framework>/.git directory
+isLocalRepo() {
+    # check if $1 repo is present ($1 = reveal impress etc..)
     echo "Testing for ${1}"
-    if [ -d ${1} ]; then
+    if [ -d "${LIBDIR}/${1}/.git" ]; then
         return 0;
     else
         return 1
     fi
 }
 
-gitupdate() {
+gitAdd() {
+    # add public repo as submodule to this repo
+
+    LIB=$1;
+    OPT=${2:-"opt"};
+    URL=${3:-"url"};
+
+    echo "git submodule add ${OPT} ${URL} ${LIBDIR}/${LIB}"
+}
+
+
+gitUpdate() {
     # Update rather than clone a repo
     # $1 = framework name from $LIBS
     echo "git pull ${LIBDIR}/${1}"
 
 }
+
 if [ $# -eq 0 ]; then
     echo "Hmm, nothing specified"
-    LIBS="reveal impress inspire s5"
+    LIBS="reveal"
 fi
 
 for LIB in ${LIBS}; do
-    gitclone $LIB "--recursive" "my-url"
-    REPO="${LIBDIR}/${LIB}/.git"
 
-    if gitexists $REPO
+    if isLocalRepo $LIB
     then
         echo "${REPO} exists!"
     else
